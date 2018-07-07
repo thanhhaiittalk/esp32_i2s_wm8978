a522dee0321e3da51e2ab6c40ce8740d3fe7d8e2
Dao Thanh Hai <thanhhai.ittalk@gmail.com>
2018-07-07 16:30:46 +0700

    esp32 i2s

---
+++ build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ /home/hai_dotcom/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp \
+ /home/hai_dotcom/Project/project_zer0_esp32/Workspace_ZERO/i2s/build/include/sdkconfig.h
+
+/home/hai_dotcom/Project/project_zer0_esp32/Workspace_ZERO/i2s/build/include/sdkconfig.h:
+/home/hai_dotcom/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
